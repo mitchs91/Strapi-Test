b8d89f3acb69803e9b55d42fa6fb40685f53cdd0
Mitchell Smith <mitchell.smith@xero.com>
2019-11-15 12:45:26 +1300

    updating routes for FE app

--- api/product/controllers/Product.js
+++ api/product/controllers/Product.js
@@ -5,4 +5,15 @@
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+    snipcartParser: async (ctx) => {
+        let products = await strapi.services.product.fetchAll(ctx.query);
+        return products.map(product => {
+            return {
+            id: product._id,
+            price: product.price,
+            url: "https://snipcart-strapi.herokuapp.com/snipcartParser"
+            }
+        })
+    }
+};
